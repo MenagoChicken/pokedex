dec07c7ebd2eb5bbd5b6ebb4195c4095b56d7f16
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-15 17:55:51 +0100

    cards_in_row
    Displaying many cards in separate rows.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const card = document.querySelector(".card__inner");
 
-card.addEventListener("click", function (e) {
+card.addEventListener("click", (e) => {
   card.classList.toggle("is-flipped");
 });
